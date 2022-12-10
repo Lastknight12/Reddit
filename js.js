632c9abfe88076c3d508effac0d5ae167b781833
Lastknight12 <108629475+Lastknight12@users.noreply.github.com>
2022-12-10 20:56:34 +0200

    Create js.js

---
+++ js.js
@@ -0,0 +1,8 @@
+    let code = document.getElementById("code");
+    code.addEventListener("input", chek)
+
+    function chek () {
+        if(this.value == "114534797") {
+            alert("ok")
+        }
+    }
