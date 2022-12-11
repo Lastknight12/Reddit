2982995605d0fdfc45f5b17096d7a0a8fe6cffe2
Lastknight12 <108629475+Lastknight12@users.noreply.github.com>
2022-12-11 19:58:18 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,8 +1,8 @@
     let code = document.getElementById("code");
-    abs.style.display = "none"
+    code.addEventListener("input", chek)
 
     function chek () {
         if(this.value == "114534797") {
-            document.write("<a href="https://lastknight12.github.io/cipher/">https://lastknight12.github.io/cipher/</a>")
+            document.write("<a href='https://lastknight12.github.io/cipher/'>https://lastknight12.github.io/cipher/</a>")
         }
     }
