251e3f7da5cc3eb84489b500c6a038a1e18c2544
Lastknight12 <108629475+Lastknight12@users.noreply.github.com>
2022-12-11 19:51:32 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,11 +1,8 @@
     let code = document.getElementById("code");
-    let abs = document.getElementById("ans")
     abs.style.display = "none"
-    code.addEventListener("input", chek)
 
     function chek () {
         if(this.value == "114534797") {
-            alert("ok")
-            abs.style.display = "block"
+            document.write("<a href="https://lastknight12.github.io/cipher/">https://lastknight12.github.io/cipher/</a>")
         }
     }
