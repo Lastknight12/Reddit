25fae3d403a815f9a21d65aa36db9fa6db06dc3d
Lastknight12 <108629475+Lastknight12@users.noreply.github.com>
2022-12-11 19:46:02 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,8 +1,11 @@
     let code = document.getElementById("code");
+    let abs = document.getElementById("ans")
+    abs.style.display = "none"
     code.addEventListener("input", chek)
 
     function chek () {
         if(this.value == "114534797") {
             alert("ok")
+            abs.style.display = "block"
         }
     }
