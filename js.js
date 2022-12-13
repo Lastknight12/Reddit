fff3607996655128949c125643c46f1cfbce6bc0
Lastknight12 <108629475+Lastknight12@users.noreply.github.com>
2022-12-13 20:14:06 +0200

    Update js.js

--- js.js
+++ js.js
@@ -2,10 +2,10 @@
     code.addEventListener("input", chek)
 
     function chek () {
-        if(this.value == "278 313 3301") {
+        if (this.value == "278 313 3301") {
             document.write("<a href='https://lastknight12.github.io/cipher/'>https://lastknight12.github.io/cipher/</a>")
         }
-        else if (this.value == "2783133301") {
+        if (this.value == "2783133301") {
             document.write("<a href='https://lastknight12.github.io/cipher/'>https://lastknight12.github.io/cipher/</a>")
         }
     }
